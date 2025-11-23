ac537b1da55099ce4c6849a210b17ab9363a969a
Le Gia Thien <gle9882@gmail.com>
2025-11-23 13:06:10 +0700

    Member3

---
+++ DoAnMaNguonMo/resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app') 
+@section('content') 
+    <div class="row"> 
+        <div class="col-md-4 mb-3"> 
+            <div class="card h-100"> 
+                <img src="https://via.placeholder.com/300x200" class="card-img-top"> 
+                <div class="card-body"> 
+                    <h5 class="card-title">Tiêu đề bài viết mẫu</h5> 
+                    <p class="card-text">Mô tả ngắn bài viết...</p> 
+                    <a href="#" class="btn btn-sm btn-outline-primary">Xem chi tiết</a> 
+                </div> 
+            </div> 
+        </div> 
+        </div> 
+@endsection 
